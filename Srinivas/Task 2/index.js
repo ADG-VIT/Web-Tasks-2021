81e07b85b34082cf6d3e256159d019117eaa1669
Srinivas Varanasi <mr7vsik@gmail.com>
2021-05-08 17:22:51 +0530

    Modified files

--- Srinivas/Task 2/index.js
+++ Srinivas/Task 2/index.js
@@ -3,23 +3,23 @@ const table = document.getElementById("table");
 let count = 0;
 
 button.addEventListener("click", ()=>{
-    const description = document.getElementById("description").value;
-    const amount = document.getElementById("money").value;
-    const date = document.getElementById("date").value;
+    const description = document.getElementById("description");
+    const amount = document.getElementById("money");
+    const date = document.getElementById("date");
     count += 1;
-    console.log(description);
-    console.log(amount);
-    console.log(date);
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
-    td1.innerText = description;
+    td1.innerText = description.value;
     let td2 = document.createElement("td");
-    td2.innerText = amount;
+    td2.innerText = amount.value;
     let td3 = document.createElement("td");
-    td3.innerText = date;
+    td3.innerText = date.value;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
-    count%2 == 0 && tr.classList.add("even");
+    count%2 == 0 ? tr.classList.add("even") : tr.classList.add("odd");
     table.appendChild(tr);
+    description.value = "";
+    amount.value = "";
+    date.value = "";
 })

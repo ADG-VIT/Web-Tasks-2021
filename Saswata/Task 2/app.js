0b97bc5f9065d2e2b1afb6171fcb0c06ed129b42
saswatabasu23 <saswatabasu23@gmail.com>
2021-05-16 19:17:18 +0530

    Tast 2 files

---
+++ Saswata/Task 2/app.js
@@ -0,0 +1,31 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+  list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+  
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+  }
+
+});
+list.addEventListener('click', e => {
+
+    if(e.target.classList.contains('delete')){
+      e.target.parentElement.remove();
+    }
+  
+  });

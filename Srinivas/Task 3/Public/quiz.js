8e38d80a9c190f5f4ed98a71ecc3c22b033268af
Srinivas Varanasi <mr7vsik@gmail.com>
2021-06-18 19:28:15 +0530

    UI improved

--- Srinivas/Task 3/Public/quiz.js
+++ Srinivas/Task 3/Public/quiz.js
@@ -1,4 +1,3 @@
-var qind = 0;
 const arr = [];
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -17,21 +16,51 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
+var obj = {};
+
 window.onload = function () {
     var fiveMinutes = 60 * 10,
         display = document.getElementById('time');
-    startTimer(fiveMinutes, display);
-    
+    const spinner = document.getElementById("spinner");
+    spinner.style.position = "absolute";
+    spinner.style.top = "0";
+    spinner.style.left = "0";
+    spinner.style.height = "100vh";
+    spinner.style.width = "100vw";
+    spinner.style.backgroundColor = "white";
+    spinner.style.opacity = "1";
     fetch("https://task-3-api.herokuapp.com/questions")
     .then(res => res.json())
     .then(data => {
-        const Q = document.getElementById("Question");
-        Q.innerText = data[qind].question;
-        const id = document.getElementById("id");
-        id.innerText = "Question "+ data[qind].questionId;
-        arr = data;
+        obj = [
+            ...data
+        ]
+        handleQuestions(data);
+        startTimer(fiveMinutes, display);
+        spinner.style.display = "none"
     })
-    .catch(err => console.log(err))
-
 };
 
+var count = 0;
+function handleQuestions(arr){
+    document.getElementById("id").innerText = `Question ${count+1}`
+    document.getElementById("Question").innerText = arr[count].question;
+    document.getElementById("option1").innerText = arr[count].optionA;
+    document.getElementById("option2").innerText = arr[count].optionB;
+    document.getElementById("option3").innerText = arr[count].optionC;
+    document.getElementById("option4").innerText = arr[count].optionD;
+}
+
+document.getElementById("prev").addEventListener("click", (e)=>{
+    if(count != 0){
+        count -= 1;
+    }
+    handleQuestions(obj)
+})
+
+document.getElementById("next").addEventListener("click", (e)=>{
+    if(count != 9){
+        count += 1;
+    }
+    handleQuestions(obj);
+})
